0d06c567c3d1deb2f39f896e59e825ef3d6bae37
Martha18 <Martha18@users.noreply.github.com>
2017-11-15 07:34:17 -0500

    Starting Work on Iris Random Forest

--- RandomForest.py
+++ RandomForest.py
@@ -6,7 +6,7 @@ import numpy as np
 np.random.seed(0)
 
 iris = load_iris()
-df =pd.DataFrame(iris.data, columns=iris.feature_names)
+df = pd.DataFrame(iris.data, columns=iris.feature_names)
 df.head()
 
 df['species'] = pd.Categorical.form_codes(iris.target, iris.target_names)
@@ -15,4 +15,15 @@ df.head()
 df['is_train'] = np.random.uniform(0, 1, len(df)) <= .75
 df.head()
 
-train, test = df[df['is_train']==True], df[]
+train, test = df[df['is_train'] == True], df[df['is_train'] == False]
+
+print('Number of observations in the training data:', len(train))
+print('Number of observations in the test dta:', len(test))
+
+features = df.colums[:4]
+features
+
+y = pd.factorize(train[species])[0]
+y
+
+
