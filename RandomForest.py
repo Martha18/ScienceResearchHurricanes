d7ba182bd1f51728230902e8bc0faf24acd1d1a2
Martha18 <Martha18@users.noreply.github.com>
2017-11-16 14:43:56 -0500

    Finished Random Forest With Iris
    Finished program and it is printing what is supposedly correct. Not sure what finial result means though.

--- RandomForest.py
+++ RandomForest.py
@@ -7,23 +7,44 @@ np.random.seed(0)
 
 iris = load_iris()
 df = pd.DataFrame(iris.data, columns=iris.feature_names)
-df.head()
+print(df.head())
 
-df['species'] = pd.Categorical.form_codes(iris.target, iris.target_names)
-df.head()
+df['species'] = pd.Categorical.from_codes(iris.target, iris.target_names)
+print(df.head())
 
 df['is_train'] = np.random.uniform(0, 1, len(df)) <= .75
-df.head()
+print(df.head())
 
 train, test = df[df['is_train'] == True], df[df['is_train'] == False]
 
 print('Number of observations in the training data:', len(train))
-print('Number of observations in the test dta:', len(test))
+print('Number of observations in the test data:', len(test))
 
-features = df.colums[:4]
-features
+features = df.columns[:4]
+print(features)
 
-y = pd.factorize(train[species])[0]
-y
+y = pd.factorize(train['species'])[0]
+print(y)
+
+clf = RandomForestClassifier(n_jobs=2, random_state=0)
+clf.fit(train[features], y)
+print(clf.fit(train[features], y))
+
+clf.predict(test[features])
+print(clf.predict(test[features]))
+
+clf.predict_proba(test[features])[0:10]
+print(clf.predict_proba(test[features])[0:10])
+
+preds = iris.target_names[clf.predict(test[features])]
+preds[0:5]
+print(preds[0:5])
+
+print(test['species'].head())
+
+pd.crosstab(test['species'], preds, rownames=['Actual Species'], colnames=['Predict Species'])
+print(pd.crosstab(test['species'], preds, rownames=['Actual Species'], colnames=['Predict Species']))
+
+
 
 
