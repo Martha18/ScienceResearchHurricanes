fa93568f147d1235a2dea622d2cdac95c00cad75
Martha18 <Martha18@users.noreply.github.com>
2017-12-19 07:42:41 -0500

    Still Can't Get scrip to work, but had an idea
    Decided I need to move mak eteh average chnage, so I am actually predicting if ERC will occur and not is it occuring

--- Script.py
+++ Script.py
@@ -1,19 +1,26 @@
-import threading
-import time
-##import urllib2
+##import threading
+##import time
+import urllib
 
-start = time.time()
-urls = ["http://www.google.com", "http://www.apple.com"]
+##start = time.time()
+##urls = ["http://www.google.com", "http://www.apple.com"]
 
-def fetch_url(url):
-    urlHandler = urllib2.urlopen(url)
-    html = urlHandler.read()
-    print ("%s\' fetched in %ss" % (url, (time.time()- start)))
+##def fetch_url(url):
+   ## urlHandler = urllib.request(url)
+   ## html = urlHandler.read()
+    ##print ("%s\' fetched in %ss" % (url, (time.time()- start)))
 
-threads = [threading.Thread(target=fetch_url, args = (url, )) for url in urls]
-for thread in threads:
-    thread.start()
-for thread in threads:
-    thread.join()
+##threads = [threading.Thread(target=fetch_url, args = (url, )) for url in urls]
 
-print ("Elapsed Time:%s" % (time.time() - start))
+
+##for thread in threads:
+    ##thread.start()
+##for thread in threads:
+    ##thread.join()
+
+##print ("Elapsed Time:%s" % (time.time() - start))*/
+
+sock = urllib.request.urlopen("http:///diveintopython.org/")
+htmlSource = sock.read()
+sock.close()
+print (htmlSource)
